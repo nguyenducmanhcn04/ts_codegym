d9e9fc27ddebddf917cd302d72761fdb5c81abf5
Nguyễn Đức Mạnh <nguyenducmanh026@gmail.com>
2024-02-17 14:01:37 +0700

    done

---
+++ spread_operator_example.ts
@@ -0,0 +1,41 @@
+
+
+
+// const cars1 = ["audi", "toyota", "motor",];
+// const cars2 = ["nisson", "kia",];
+
+// const cars3 = [...cars1, ...cars2];
+
+// console.log(cars3);
+
+// const cars1 = {
+//     brand: "bmw",
+//     color: "black",
+// }
+
+// const cars2 = {...cars1};
+
+// console.log(cars2);
+
+
+// const cars1 = {
+//     brand: "bmw",
+//     color: "black",
+// }
+
+// const cars2 = {
+//     brand: "nisson",
+//     color: "red",
+//     year: 2020,
+// }
+// const cars3 = {...cars1, ...cars2};
+
+// console.log(cars3);
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// const [a, b, c, ...other] = numbers;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(other);
